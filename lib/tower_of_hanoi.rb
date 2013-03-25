aa9e9e2277cc9cb6e51eb68acf5e1145d7dd41ee
Mark and Ian <mark+ian@appacademy.io>
2013-03-25 15:10:11 -0700

    Modify error message and fix spacing

--- lib/tower_of_hanoi.rb
+++ lib/tower_of_hanoi.rb
@@ -1,8 +1,8 @@
 class TowerOfHanoi
   attr_reader :rods
 
-  def self.new_game(num_disks=3)
-    rods = Array.new(3) {Array.new}
+  def self.new_game(num_disks = 3)
+    rods = Array.new(3) { [] }
     num_disks.downto(1).each { |disk| rods[0] << disk }
 
     TowerOfHanoi.new(rods)
@@ -19,13 +19,13 @@ class TowerOfHanoi
   end
 
   def valid_move?(from, to)
-    raise "InvalidMove" unless [from, to].all? { |rod_num| rod_num.between?(0, 2) }
+    raise "invalid move" unless [from, to].all? { |rod_num| rod_num.between?(0, 2) }
     if @rods[from].empty?
-      raise "InvalidMove"
+      raise "invalid move"
     elsif @rods[to].empty?
       true
     else
-      raise "InvalidMove" if @rods[to].last < @rods[from].last
+      raise "invalid move" if @rods[to].last < @rods[from].last
       true
     end
   end
--- spec/tower_of_hanoi_spec.rb
+++ spec/tower_of_hanoi_spec.rb
@@ -9,21 +9,21 @@ describe TowerOfHanoi do
   end
 
   it "disks are moveable" do
-    subject.move_disk(0,2)
-    subject.rods.should == [[3,2],[],[1]]
+    subject.move_disk(0, 2)
+    subject.rods.should == [[3, 2], [], [1]]
   end
 
   it "can't place a larger disk on a smaller disk" do
     subject.move_disk(0, 2)
-    expect { subject.move_disk(0, 2) }.to raise_error("InvalidMove")
+    expect { subject.move_disk(0, 2) }.to raise_error("invalid move")
   end
 
   it "can't move a disk from empty rod" do
-    expect { subject.move_disk(2, 0) }.to raise_error("InvalidMove")
+    expect { subject.move_disk(2, 0) }.to raise_error("invalid move")
   end
 
   it "can't move disk to nonexistant rod" do
-    expect { subject.move_disk(0,3) }.to raise_error("InvalidMove")
+    expect { subject.move_disk(0, 3) }.to raise_error("invalid move")
   end
 
   context "winning game" do
