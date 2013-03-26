3a1935e3850bb259f5e32fcbf23bfdd7afd96619
Mark and Ian <mark+ian@appacademy.io>
2013-03-26 16:33:34 -0700

    Add test for opening sequence

--- reversi/lib/board.rb
+++ reversi/lib/board.rb
@@ -1,21 +1,74 @@
 require_relative 'piece'
 
 class Board
+
+  OFFSETS = [[-1, -1],
+             [ 0, -1],
+             [ 1, -1],
+             [-1,  0],
+             [ 1,  0],
+             [-1,  1],
+             [ 0,  1],
+             [ 1,  1]]
+
   def self.new_board
-    Array.new(8) { Array.new(8) }
+    board = Array.new(8) { Array.new(8) }
+    board[3][3, 2] = [Piece.new(:white), Piece.new(:black)]
+    board[4][3, 2] = [Piece.new(:black), Piece.new(:white)]
+    board
   end
 
   def initialize(board = Board.new_board)
     @board = board #why is a board object being initialized to a board object??
   end
 
-  def make_piece(color)
+  def new_piece(color)
     Piece.new(color)
   end
 
   def set_piece(piece, coord)
     @board[coord[0]][coord[1]] = piece
     # @board[*coord] = piece # why doesn't this work?
+  end
+
+  def valid_move?(color, coord)
+    return false if off_board?(coord) || self[*coord].is_a?(Piece)
+    return false if adjacent_squares(coord).all?(&:nil?)
+
+  end
+
+  def coords_to_flip(piece, coord)
+    coords_to_flip = []
+
+    OFFSETS.each do |offset|
+      current_coord = coord
+      loop do
+        current_coord = next_coord(current_coord, offset)
+        content = self[*current_coord]
+        current_color = content.is_a?(Piece) ? content.color : nil
+        break if off_board?(current_coord) ||
+                 [piece.color, nil].include?(current_color)
+
+        coords_to_flip << current_coord
+      end
+    end
+
+    coords_to_flip
+  end
+
+  def adjacent_squares(coord)
+    OFFSETS.map do |offset|
+      neighbor = next_coord(coord, offset)
+      self[*neighbor]
+    end
+  end
+
+  def next_coord(coord, offset) # GET BETTER NAME
+    [coord[0] + offset[0], coord[1] + offset[1]]
+  end
+
+  def off_board?(coord)
+    coord.any? { |x| !x.between?(0, 7) }
   end
 
   def [](*coord)
--- reversi/spec/board_spec.rb
+++ reversi/spec/board_spec.rb
@@ -9,6 +9,13 @@ describe Board do
     end
   end
 
+  describe "square content retrieval" do
+    it "retrieves a square's contents" do
+      subject[4, 4].should be_a(Piece)
+      subject[5, 5].should be_nil
+    end
+  end
+
   describe "#set_piece" do
     let(:black_piece) { double('piece', :color => :black ) }
 
@@ -18,10 +25,42 @@ describe Board do
     end
   end
 
-  it "checks valid moves"
+  describe "#valid_move?" do
+    # subject
+    let(:off_board) { [9, 9] }
+    let(:taken)     { [4, 4] }
+    let(:isolated)  { [1, 1] }
+    let(:no_flip)   { [5, 2] }
 
+    it "rejects a coord off the board" do
+      subject.valid_move?(:black, off_board).should equal(false)
+    end
 
-  it "makes flips"
+    it "rejects a taken square" do
+      subject.valid_move?(:black, taken).should equal(false)
+    end
+
+    it "rejects an isolated piece" do
+      subject.valid_move?(:black, isolated).should equal(false)
+    end
+  end
+
+  context "opening sequence" do
+    let(:coord1) { [2, 3] }
+    let(:coord2) { [4, 2] }
+    let(:piece1) { double("piece", :color => :black) }
+    let(:piece2) { double("piece", :color => :white) }
+
+    it "first move flips correct piece" do
+      subject.coords_to_flip(piece1, coord1).should == [[3, 3]]
+    end
+
+    it "second move flips correct piece" do
+      subject.set_piece(piece1, coord1)
+      subject.coords_to_flip(piece2, coord2).should == [[4, 3]]
+    end
+  end
+
   it "multiple directions"
   it "win"
 
